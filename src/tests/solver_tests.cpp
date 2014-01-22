e1c4eb10b9c249731d348617b62cc49d3f148b6c
Ben Miller <benmiller@Bens-MacBook-Pro.local>
2014-01-22 16:33:28 -0700

    cleaned up some test code in the solver test that isnt needed.

--- src/tests/solver_tests.cpp
+++ src/tests/solver_tests.cpp
@@ -4,19 +4,7 @@
 #include "../hand.h"
 #include "../card.h"
 
-#include <iostream>
-#include <string>
-#include <sstream>
-
 using namespace cribslvr;
-
-std::string printVect(std::vector<Card> vect){
-	std::stringstream ret;
-	for(std::vector<Card>::iterator i = vect.begin(); i!= vect.end(); i++){
-		ret << " " << i->print(); 
-	}
-	return ret.str();
-}
 
 SUITE(SolverTests)
 {
@@ -25,13 +13,12 @@ SUITE(SolverTests)
 		CHECK_EQUAL(52, Solver::all_cards.size());
 	}
 
-	TEST(ProbabilitiesTest)
+	TEST(PossibilitiesTest)
 	{
 		Card cards[6] = {Card("S1"), Card("S2"), Card("S3"), Card("S10"), Card("C6"), Card("C13")};
 		Hand hand(cards);
 		hand.discard(Card("C6"), Card("C13"));
 		Solver solver(hand); 
-		std::cout << "test" <<std::endl;
 		PossibilityMap probabilites = solver.findScoringPossibilites();
 		CHECK_EQUAL(probabilites[1].size(), 0);
 		CHECK_EQUAL(probabilites[5].size(), 9);
@@ -44,4 +31,6 @@ SUITE(SolverTests)
 		CHECK_EQUAL(probabilites[14].size(), 3);
 		CHECK_EQUAL(probabilites[15].size(), 0);
 	}
+
+
 }
